aa49eed19e34352602e76102d017fb82c9f02a19
MarilenaRoque <marilenas@id.uff.br>
2020-11-10 18:51:34 -0300

    Finishing Front-end Template
    - Set buttons to toggle temperature
    - Set display for informations
    - styling buttons and forms

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import getData from './weatherData';
 // Testings!
 console.log("I am working perfect splendid!");
 console.log("watch is set!");
-getData('london');
+getData('lasdadsjfhasçdfháouhfpaiudsfhp');
 // Testing!
